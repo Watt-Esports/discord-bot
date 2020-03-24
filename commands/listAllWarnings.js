3a813a2630558fd8ce1afd77a837919baebd2855
NightwingEU <danial.qureshi@hotmail.co.uk>
2020-03-24 17:26:58 +0000

    Updated code for google spread sheet v4

--- commands/listAllWarnings.js
+++ commands/listAllWarnings.js
@@ -1,4 +1,4 @@
-const GoogleSpreadsheet = require('google-spreadsheet');
+const {GoogleSpreadsheet} = require('google-spreadsheet');
 
 module.exports = {
 	name: 'allwarnings',
@@ -7,31 +7,39 @@ module.exports = {
 	modOnly: true,
 	adminOnly: true,
 	usage: 'allwarnings',
-	execute(message) {
+	async execute(message) {
 		const {channelIDs, spreadsheetID, spreadsheetConfig} = message.client.config;
 		const spreadsheet = new GoogleSpreadsheet(spreadsheetID);
 		const memberIdArray = [];
 		const counts = {};
-		let logMessage = 'Below are the warnings with counts \n';
+		let logMessage = 'Below are the warnings with counts\n';
 
-		spreadsheet.useServiceAccountAuth(spreadsheetConfig, () => {
-			spreadsheet.getRows(3, (err, rows) => {
-				for (const row of rows) {
-					memberIdArray.push(row.memberid);
-				}
-
-				memberIdArray.forEach((index) => {
-					counts[index] = (counts[index] || 0) + 1;
-				});
-
-				for (const entry of Object.entries(counts)) {
-					const member = message.guild.members.get(entry[0]);
-
-					logMessage += `${member} has ${entry[1]} warning(s) \n`;
-				}
-
-				message.client.channels.get(channelIDs.adminLogging).send(logMessage);
-			});
+		await spreadsheet.useServiceAccountAuth({
+			/* eslint-disable-next-line camelcase */
+			client_email: spreadsheetConfig.client_email,
+			/* eslint-disable-next-line camelcase */
+			private_key: spreadsheetConfig.private_key
 		});
+
+		await spreadsheet.loadInfo();
+
+		const warningSheet = spreadsheet.sheetsByIndex[2];
+		const warningRows = await warningSheet.getRows();
+
+		for (const warningRow of warningRows) {
+			memberIdArray.push(warningRow.MemberID);
+		}
+
+		memberIdArray.forEach((index) => {
+			counts[index] = (counts[index] || 0) + 1;
+		});
+
+		for (const entry of Object.entries(counts)) {
+			const member = message.guild.members.get(entry[0]);
+
+			logMessage += `${member} has ${entry[1]} warning(s)\n`;
+		}
+
+		message.client.channels.get(channelIDs.adminLogging).send(logMessage);
 	}
 };
--- commands/membershipVerification.js
+++ commands/membershipVerification.js
@@ -1,4 +1,4 @@
-const GoogleSpreadsheet = require('google-spreadsheet');
+const {GoogleSpreadsheet} = require('google-spreadsheet');
 
 module.exports = {
 	name: 'member',
@@ -6,7 +6,7 @@ module.exports = {
 	guildOnly: true,
 	helpMsg: true,
 	usage: 'member <MatriculationNumber>',
-	execute(message, args) {
+	async execute(message, args) {
 		const member = message.member;
 		const hwID = args[0].toUpperCase();
 		const {roleIDs, spreadsheetID, spreadsheetConfig} = message.client.config;
@@ -14,40 +14,39 @@ module.exports = {
 		message.delete();
 		if (hwID.length === 9 && hwID.startsWith('H')) {
 			const spreadsheet = new GoogleSpreadsheet(spreadsheetID);
+			let hwIDInSheet = false;
 
-			spreadsheet.useServiceAccountAuth(spreadsheetConfig, () => {
-				spreadsheet.getRows(1, (err, rows) => {
-					let hwIDInSheet = false;
-
-					for (const row of rows) {
-						if (row.matriculationnumber === hwID) {
-							hwIDInSheet = true;
-							row.discordname = member.user.username + '#' + member.user.discriminator;
-							row.updated = 'Yes';
-							row.save();
-							member.addRole(roleIDs.socMember);
-							break;
-						}
-					}
-
-					if (!hwIDInSheet) {
-						const rowToAdd = {
-							'matriculation number': hwID,
-							'discordid': member.id
-						};
-
-						spreadsheet.addRow(2, rowToAdd, () => {
-							console.log('Added to Bot Pending');
-						});
-					}
-
-					if (err) {
-						console.log(err);
-					}
-				});
+			await spreadsheet.useServiceAccountAuth({
+				/* eslint-disable-next-line camelcase */
+				client_email: spreadsheetConfig.client_email,
+				/* eslint-disable-next-line camelcase */
+				private_key: spreadsheetConfig.private_key
 			});
-		} else {
-			message.channel.send(member + ' Incorrect Matriculation Number');
+
+			await spreadsheet.loadInfo();
+
+			const idSheet = spreadsheet.sheetsByIndex[0];
+			const rows = await idSheet.getRows();
+
+			for (const row of rows) {
+				if (row.MatriculationNumber == hwID) {
+					hwIDInSheet = true;
+					row.DiscordName = member.user.username + '#' + member.user.discriminator;
+					row.Updated = 'Yes';
+					await row.save();
+					member.addRole(roleIDs.socMember);
+					break;
+				}
+			}
+
+			if (!hwIDInSheet) {
+				const pendingSheet = spreadsheet.sheetsByIndex[1];
+
+				await pendingSheet.addRow({
+					'MatriculationNumber': hwID,
+					'DiscordID': member.id
+				});
+			}
 		}
 	}
 };
