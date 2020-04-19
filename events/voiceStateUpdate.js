5535fbda210f2a6c33b44d05584a56b9dac8dfe8
ZyerTCoder <protekthorwow@gmail.com>
2020-04-19 03:05:55 +0100

    added vcSpam text channel being shows/hidden when joining/leaving vcs

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,6 +1,9 @@
 module.exports = async (client, oldState, newState) => {
-	const stackVCIDs = client.config.stackVCIDs;
 
+	const {channelIDs, stackVCIDs} = client.config;
+	const vcSpam = client.channels.get(channelIDs['vcSpam']);
+
+	// if (client.settings.autoStackResizer) {
 	// To increase stack names when over filled
 	if (newState.voiceChannelID in stackVCIDs) {
 		const intendedStackSize = stackVCIDs[newState.voiceChannelID];
@@ -21,4 +24,17 @@ module.exports = async (client, oldState, newState) => {
 			await oldState.voiceChannel.setName(`${oldState.voiceChannel.members.size} Stack`);
 		}
 	}
+	// }
+
+	// if (client.settings.vcSpamToggle) {
+	// Give read perms on vcSpam when someone joins
+	if (!oldState.voiceChannelID) {
+		vcSpam.overwritePermissions(oldState.user, {VIEW_CHANNEL: true});
+	}
+	// Remove read perms on vcSpam when someone leaves, note: this will completely remove that users overwrite perms
+	if (!newState.voiceChannelID) {
+		vcSpam.permissionOverwrites.get(newState.user.id).delete();
+	}
+
+	// }
 };
