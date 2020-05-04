9e1727774b1b27e95f1ef5c7f16ad3e3ef6a14c1
Peter <simber2@btinternet.com>
2020-05-04 02:12:31 +0100

    Added DM and Number checks
    it now checks it has a number at the start and deletes it if it's not DM.

--- commands/anonSuggest.js
+++ commands/anonSuggest.js
@@ -1,3 +1,5 @@
+const {GuildChannel} = require('discord.js');
+
 module.exports = {
 	name: 'anonsuggest',
 	description: 'Posts an anonymous Suggestion',
@@ -7,10 +9,31 @@ module.exports = {
 		const {modBot} = message.client.config.channelIDs;
 		const suggestion = args.join(' ');
 
+		const lastChar = args[0].slice(args[0].length - 1, args[0].length);
+		let number;
+
+		if (!isNaN(lastChar)) {
+			number = args[0];
+		}else {
+			number = args[0].slice(0, args[0].length - 1);
+		}
+
+		if (isNaN(number)) {
+			message.author.send('Please put a number at the start');
+			if (message.channel instanceof GuildChannel) {
+				message.delete();
+			}
+		}
+
 		message.client.channels.get(modBot).send('"' + suggestion + '": ' + message.author)
 			.then(async sent => {
 				await sent.react('✔');
 				await sent.react('❌');
 			});
+
+		message.author.send('Your suggestion has been submitted pending approval!');
+		if (message.channel instanceof GuildChannel) {
+			message.delete();
+		}
 	}
 };
