cf87654809a27745233878d6f3e0fadfeb6aeca3
NightwingEU <danial.qureshi@hotmail.co.uk>
2020-09-12 23:45:42 +0100

    Removing reaction role stuff

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,32 +1,7 @@
-const messageMap = require('../utils/reactRoleMap');
 const {messageReactionAdd} = require('../utils/commandToggles.json');
 
 module.exports = (client, reaction, user) => {
 	if (messageReactionAdd) {
-		const {guildID} = client.config;
-		const guildObj = client.guilds.cache.get(guildID);
-
-		for (const messageMapKey of Object.keys(messageMap)) {
-			// Message ID is stored as string since it's too big a number to store
-			if (messageMapKey === reaction.message.id.toString()) {
-				for (const roleMap of messageMap[messageMapKey]) {
-					if (reaction.emoji.name === roleMap.emoji) {
-						guildObj.members.forEach((member) => {
-							if (member.id === user.id) {
-								const roleToAdd = guildObj.roles.find(r => r.name === roleMap.role);
-
-								member.addRole(roleToAdd);
-								if (roleToAdd.name === 'Members' || roleToAdd.name === 'HW Members') {
-									member.addRole(guildObj.roles.find(r => r.name === 'LFG'));
-								}
-							}
-						});
-					}
-				}
-			}
-		}
-
-
 		// AnonSuggestion reaction listening
 		const {moderation, suggestions} = client.config.channelIDs;
 
