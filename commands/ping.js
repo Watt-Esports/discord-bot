12af2988ea9b17e0017a7ea4cb9c50ffa44203b1
Calum Felstead <cf2007@hw.ac.uk>
2020-09-17 13:12:54 +0100

    Test commit

---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+module.exports = {
+	name: 'ping',
+	description: 'Short description of what the command does',
+	guildOnly: true, // Boolean, is not necessary depending on the command
+	usage: 'example usage of the command using <> to show variables',
+	execute(message, args) {
+
+
+		// code goes here
+
+	}
+};
