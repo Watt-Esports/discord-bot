42f7d9a9f2401df0e36833c289d63d0c80611f42
Calum Felstead <cf2007@hw.ac.uk>
2020-09-17 13:16:17 +0100

    Test commit revert pepe change

--- commands/pepe.js
+++ commands/pepe.js
@@ -1,5 +1,5 @@
 const pepeDatabase = require('../utils/pepeDatabase.js');
-const {MessageAttachment} = require('discord.js');
+const { Attachment } = require('discord.js');
 
 module.exports = {
 	name: 'pepe',
@@ -9,7 +9,7 @@ module.exports = {
 	execute(message) {
 		const randomPepe = pepeDatabase[Math.floor(Math.random() * pepeDatabase.length)];
 		const pngPepe = randomPepe.slice(0, -1) + '.png';
-		const pepeAttachment = new MessageAttachment(pngPepe);
+		const pepeAttachment = new Attachment(pngPepe);
 
 		message.channel.send(pepeAttachment);
 	}
